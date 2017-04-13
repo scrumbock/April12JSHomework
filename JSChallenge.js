41f9dde5c096ebca643451ec494d14b675c2585f
scrumbock <scrumboc@yahoo.com>
2017-04-12 20:38:59 -0400

    solved challenge #3 part two

--- JSChallenge.js
+++ JSChallenge.js
@@ -22,3 +22,22 @@ var bandsAreCool = [bandGenres, bandNames];
 console.log(bandsAreCool[0][2])
 console.log(bandsAreCool[1][2])
 
+// 3.) Write a script that checks if a variable captured from a prompt is less than 100. If it is, alert the user that their variable is less than 100. If it is not alert the user of what the value was and that it was greater than 100.
+
+
+function getNumber() {
+
+var num = document.getElementById("number").value;
+var button = document.getElementById("numButton");
+
+	if (num < 100) {
+	alert("Your number is less than 100!")
+	} 
+	else if (num > 100) {
+	alert("Your number was" + " " + num + " " + "and that is more than 100!")
+	}
+}
+	document.getElementById("numButton").addEventListener("click", getNumber);
+
+
+
