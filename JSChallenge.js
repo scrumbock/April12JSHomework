958aa0c7b5f4fbed4f8cdd22f3e3a7fde2bbc74e
scrumbock <scrumboc@yahoo.com>
2017-04-12 21:07:19 -0400

    solved challenge #6, working out of order

--- JSChallenge.js
+++ JSChallenge.js
@@ -6,7 +6,7 @@ var red = 6
 
 console.log(blue + red) 
 
-// 1 part 2 ) Now do the same with two strings.
+// // 1 part 2 ) Now do the same with two strings.
 
 var blue = ("I am a hard worker,")
 var red = (" but a slow learner...")
@@ -15,14 +15,14 @@ console.log(blue + red)
 
 // // 2.) Create a multidimensional array related to a subject that interests you. Output two of the items in sub-arrays to the console.
 
-var bandGenres = ["Punk", "Post-Punk", "Psychedelic", "Industrial"];
-var bandNames = ["Buzzcocks", "Joy Division", "King Gizzard", "Ministry"];
+var bandGenres = ["Garage", "Post-Punk", "Psychedelic", "Industrial"];
+var bandNames = ["Kinks", "Joy Division", "King Gizzard and the Lizard Wizard", "Ministry"];
 var bandsAreCool = [bandGenres, bandNames];
 
 console.log(bandsAreCool[0][2])
 console.log(bandsAreCool[1][2])
 
-// 3.) Write a script that checks if a variable captured from a prompt is less than 100. If it is, alert the user that their variable is less than 100. If it is not alert the user of what the value was and that it was greater than 100.
+// // 3.) Write a script that checks if a variable captured from a prompt is less than 100. If it is, alert the user that their variable is less than 100. If it is not alert the user of what the value was and that it was greater than 100.
 
 
 function getNumber() {
@@ -40,4 +40,24 @@ var button = document.getElementById("numButton");
 	document.getElementById("numButton").addEventListener("click", getNumber);
 
 
+// 4.) Try running the script and then changing the variable's value to see how this affects the program's output.
+
+// ANSWER: I don't know how to demonstrate this in the file other than inserting different variables in the form on the browser page?
+
+
+// 5.) Write a similar script to check if a string stored in a variable is the same as another string.
+
+// function getInfo() {
+// 	var 
+// }
+
+
+// 6.) Declare a function that takes a name as an argument and tells the user what name they've entered. Try running it after it has been declared.
+
+
+function alertName(somebodysName){
+	return alert("You have entered " + somebodysName)
+}
+
+alertName("George")
 
