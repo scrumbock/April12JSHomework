37f68e0c2e5c6f073468a73c7325aed3c371120b
scrumbock <scrumboc@yahoo.com>
2017-04-12 20:32:44 -0400

    solved first challenge

---
+++ JSChallenge.js
@@ -0,0 +1,15 @@
+// //  1.) Create a script with two variables, each assigned to a number. Add them together and output the result to the console. 
+
+
+var blue = 2
+var red = 6
+
+console.log(blue + red) 
+
+// 1 part 2 ) Now do the same with two strings.
+
+var blue = ("I am a hard worker,")
+var red = (" but a slow learner...")
+
+console.log(blue + red)
+
